b8e8b3f563c3621dec3f0f79ab8d92e96d896fd0
MilanoTester <eliasmilano.tester@gmail.com>
2021-07-14 00:45:55 -0300

    Práctico 4, ejercicio 3

---
+++ src/practico4_ej3.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class practico4_ej3 {
+    public static void main (String args[]) {
+        // Escribir un método que reciba un número y retorne su doble
+
+        Scanner input = new Scanner(System.in);
+        int nUsuario=0;
+        int nDoble = 0;
+
+        System.out.println("El programa calcula el doble de un número ingresado por el usuario.");
+        System.out.println("Ingrese un número:");
+        nUsuario = input.nextInt();
+
+        nDoble = dobleNum(nUsuario);
+        System.out.println("El doble del número ingresado es: " + nDoble);
+        fin();
+
+    }
+        // Calcular el doble de un número
+        public static int dobleNum (int num) {
+            return num * 2;
+        }
+
+        // Mensaje fin del programa
+        public static void fin() {
+            System.out.println("Fin.");
+        }
+
+}
