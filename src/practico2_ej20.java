b491267813248f3ad90f5660aa2fed6870111538
MilanoTester <eliasmilano.tester@gmail.com>
2021-07-16 20:40:28 -0300

    práctico 2, ejercicio 20

---
+++ src/practico2_ej20.java
@@ -0,0 +1,14 @@
+public class practico2_ej20 {
+    public static void main (String args[]) {
+        // Programa Java que muestre los números del 1 al 10 utilizando la instrucción for
+
+        //Presentación del programa
+        System.out.println("El programa muestra en pantalla los números desde 1 hasta 10.");
+
+        for (int i=1; i<=10; i++) {
+            System.out.println(i);
+        }
+
+        System.out.println("El programa ha finalizado.");
+    }
+}
