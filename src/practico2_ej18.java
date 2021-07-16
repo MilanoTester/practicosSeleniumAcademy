66ce6b20947c1826a29d747a54ba3a4a823515b4
MilanoTester <eliasmilano.tester@gmail.com>
2021-07-16 20:35:22 -0300

    práctico 2, ejercicio 18

---
+++ src/practico2_ej18.java
@@ -0,0 +1,14 @@
+public class practico2_ej18 {
+    public static void main (String args[]) {
+        // Imprimir los números del 10 al 1000, de 10 en 10.
+
+        //Descripción del programa
+        System.out.println("El programa muestra en pantalla los números desde 10 hasta 1000, en incrementos de 10 en 10.");
+
+        for (int i=10; i<=1000; i=i+10) {
+            System.out.println(i);
+        }
+
+        System.out.println("El programa ha finalizado.");
+    }
+}
