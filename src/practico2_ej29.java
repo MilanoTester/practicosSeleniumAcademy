d19b3e36629693b78ad14b35f2271b8b3041fb24
MilanoTester <eliasmilano.tester@gmail.com>
2021-07-16 20:51:38 -0300

    práctico 2, ejercicio 29

---
+++ src/practico2_ej29.java
@@ -0,0 +1,14 @@
+public class practico2_ej29 {
+    public static void main (String args[]) {
+        // Programa Java que muestre los números del 1 al 10 utilizando la instrucción while
+
+        System.out.println("El programa muestra los números desde 1 hasta 10:");
+        int i=1;
+
+        while (i<=10) {
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("Fin.");
+    }
+}
