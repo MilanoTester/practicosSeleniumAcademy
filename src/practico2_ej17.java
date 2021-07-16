56e94bd11f482c7c0a5bc0ea9a9608c36034aecc
MilanoTester <eliasmilano.tester@gmail.com>
2021-07-16 20:34:02 -0300

    práctico 2, ejercicio 17

---
+++ src/practico2_ej17.java
@@ -0,0 +1,15 @@
+public class practico2_ej17 {
+    public static void main (String args[]) {
+        //Imprimir los números del 1 al 10.
+
+        //Descripción del programa
+        System.out.println("El programa muestra en pantalla los números desde el 1 hasta el 10:");
+
+        for (int i=1; i<=10; i++) {
+            System.out.println(i);
+        }
+
+        System.out.println("El programa ha finalizado.");
+    }
+}
+
