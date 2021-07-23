7140a817e4d6d947cc2c2aa000477e1fa8af77bb
MilanoTester <eliasmilano.tester@gmail.com>
2021-07-23 00:50:41 -0300

    práctico 6, ejercicios de Arreglos - avance 1

---
+++ src/practico6/EjerciciosArreglos.java
@@ -0,0 +1,19 @@
+package practico6;
+
+import java.util.Scanner;
+
+public class EjerciciosArreglos {
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+
+        // 1. Calcular la media de una serie de números que se leen por teclado.
+        
+
+
+
+    }
+
+
+
+
+}
