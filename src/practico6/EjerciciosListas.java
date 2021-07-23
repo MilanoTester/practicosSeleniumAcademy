211999c4f944f954f9e7089bffba57ebb689db07
MilanoTester <eliasmilano.tester@gmail.com>
2021-07-22 22:52:52 -0300

    práctico 6, ejercicios Lista - terminado. agregado a archivo gitignore: extension *.xml

--- src/practico6/EjerciciosListas.java
+++ src/practico6/EjerciciosListas.java
@@ -1,13 +1,13 @@
 package practico6;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class EjerciciosListas {
     public static void main(String args []) {
 
         // Crear listas
         ArrayList<Integer> listaDatos = new ArrayList<Integer>();
-        ArrayList<String> listaColores = new ArrayList<String>();
 
         // Agregar datos a las listas
         listaDatos.add(10);
@@ -17,54 +17,78 @@ public class EjerciciosListas {
         listaDatos.add(50);
         listaDatos.add(-3);
 
-        listaColores.add("Azul");
-        listaColores.add("Verde");
-        listaColores.add("Naranja");
-        listaColores.add("Rojo");
-        listaColores.add("Púrpura");
-
-        // Imprimir datos de las listas
-        System.out.println("Imprimiendo lista en pantalla (usando un sout): " + listaDatos);
-        System.out.println("Imprimiendo lista en pantalla (usando un sout): " + listaColores);
 
         // 3. Crear un método imprimirListaEnteros que reciba una lista de enteros, y los imprima en pantalla
         imprimirListaEnteros(listaDatos);
 
         // 4. Crear un método imprimirListaStrings que reciba una lista de Strings, y los imprima en pantalla
+        ArrayList<String> listaColores = new ArrayList<String>();
+        listaColores.add("Azul");
+        listaColores.add("Verde");
+        listaColores.add("Naranja");
+        listaColores.add("Rojo");
+        listaColores.add("Púrpura");
         imprimirListaStrings(listaColores);
 
         // 5. Método que reciba una lista de enteros, y retorne la cantidad de elementos de la misma
         int tamañoListaDatos = cantidadElementosEnteros(listaDatos);
-        System.out.println("Cantidad de elementos en la lista de números enteros: " + tamañoListaDatos);
+        System.out.println("Ejercicio 5. Cantidad de elementos en la lista de números enteros: " + tamañoListaDatos);
 
         // 6. Método que reciba una lista de enteros, y retorne verdadero si y sólo si la lista está vacía
         boolean listaDatosVacia = listaEnterosVacia(listaDatos);
-        System.out.println("La lista de números enteros esta vacía? " + listaDatosVacia);
+        System.out.println("Ejercicio 6. La lista de números enteros esta vacía? " + listaDatosVacia);
 
         // 7. Método que reciba una lista de enteros, y retorne la suma
         int sumaListaDatos = sumaListaEnteros(listaDatos);
-        System.out.println("La suma de la lista de números enteros es: " + sumaListaDatos);
+        System.out.println("Ejercicio 7. La suma de la lista de números enteros es: " + sumaListaDatos);
 
         // 8. Método que reciba una lista de enteros, y retorne el promedio de los mismos
         float promedioListaDatos = promedioListaEnteros(listaDatos);
-        System.out.println("El promedio de los números enteros es: " + promedioListaDatos);
+        System.out.println("Ejercicio 8. El promedio de los números enteros es: " + promedioListaDatos);
 
         // 9. Método que reciba una lista de enteros, y retorne el menor
         int menorListaDatos = menorListaEnteros(listaDatos);
-        System.out.println("El menor valor de la lista de números enteros es: " + menorListaDatos);
+        System.out.println("Ejercicio 9. El menor valor de la lista de números enteros es: " + menorListaDatos);
 
         // 10. Método que reciba una lista de enteros, y retorne el mayor
         int mayorListaDatos = mayorListaEnteros(listaDatos);
-        System.out.println("El mayor valor de la lista de números enteros es: " + mayorListaDatos);
+        System.out.println("Ejercicio 10. El mayor valor de la lista de números enteros es: " + mayorListaDatos);
 
+        // 11. Método que reciba una lista de booleanos, y retorne verdadero si y sólo si todos los elementos de la lista son true.
+        // De lo contrario se debe retornar false. Sugerencia: utilizar WHILE.
+        ArrayList<Boolean> listaPreguntas = new ArrayList<Boolean>();
+        listaPreguntas.add(true);
+        listaPreguntas.add(false);
+        listaPreguntas.add(true);
+        listaPreguntas.add(true);
+        listaPreguntas.add(true);
+        boolean trueListaPreguntas = trueListaBooleanos(listaPreguntas);
 
+        if (listaPreguntas.isEmpty() == false) {
+            if (trueListaPreguntas == true) {
+                System.out.println("Ejercicio 11. Todos los valores de la lista de preguntas son verdaderos, por lo tanto el resultado es verdadero.");
+            } else {
+                System.out.println("Ejercicio 11. Al menos un valor de la lista de preguntas es falso, por lo tanto el resultado es falso.");
+            }
+        } else {
+            System.out.println("Ejercicio 11. La lista de preguntas esta vacía, no contiene ningún valor.");
+        }
 
-        // PROBAR QUE SUCEDE CON EL TAMAÑO DE LA LISTA SI ELIMINO UN ELEMENTO? (EL TAMAÑO DISMINUYE?)
+        // 12. Método leerValores(): pide por teclado los números y los almacena en el ArrayList. La lectura acaba cuando se introduce
+        // el valor -99. El método devuelve mediante return el ArrayList con los valores introducidos
+        System.out.println("Ejercicio 12. El sistema acepta el ingreso de números para generar una lista.");
+        ArrayList<Float>listaNumeros = leerValores();
+        System.out.println("La lista de números ingresados es: " + listaNumeros);
+
+        // FIN DEL PROGRAMA
+        System.out.println("Fin");
+
     }
+
 
     // 3. Crear un método imprimirListaEnteros que reciba una lista de enteros, y los imprima en pantalla
     public static void imprimirListaEnteros(ArrayList<Integer> listaEnteros) {
-        System.out.println("Estos son los datos de la lista de enteros:");
+        System.out.println("Ejercicio 3. Estos son los datos de la lista de enteros:");
         for (int i=0; i<listaEnteros.size(); i++) {
             System.out.println(listaEnteros.get(i));
         }
@@ -72,7 +96,7 @@ public class EjerciciosListas {
 
     // 4. Crear un método imprimirListaStrings que reciba una lista de Strings, y los imprima en pantalla
     public static void imprimirListaStrings(ArrayList<String> listaStrings) {
-        System.out.println("Estos son los datos de la lista de Strings:");
+        System.out.println("Ejercicio 4. Estos son los datos de la lista de Strings:");
         for (int i=0; i<listaStrings.size(); i++) {
             System.out.println(listaStrings.get(i));
         }
@@ -85,13 +109,11 @@ public class EjerciciosListas {
 
     // 6. Método que reciba una lista de enteros, y retorne verdadero si y sólo si la lista está vacía
     public static boolean listaEnterosVacia(ArrayList<Integer> listaEnteros) {
-        boolean listaVacia;
         if (listaEnteros.isEmpty() == true) {
-            listaVacia = true;
+            return true;
         } else {
-            listaVacia = false;
+            return false;
         }
-        return listaVacia;
 
     }
 
@@ -132,5 +154,37 @@ public class EjerciciosListas {
         return mayor;
     }
 
+    // 11. Método que reciba una lista de booleanos, y retorne verdadero si y sólo si todos los elementos de la lista son true.
+    // De lo contrario se debe retornar false. Sugerencia: utilizar WHILE.
+    public static boolean trueListaBooleanos(ArrayList<Boolean> listaBooleanos) {
+        boolean trueBooleanos = true;
+        int i = 0;
+        while ((i < listaBooleanos.size()) && (trueBooleanos == true)) {
+            if (listaBooleanos.get(i) == false) {
+                trueBooleanos = listaBooleanos.get(i);
+            }
+            i++;
+        }
+        return trueBooleanos;
+    }
+
+    // 12. Método leerValores(): pide por teclado los números y los almacena en el ArrayList. La lectura acaba cuando se introduce
+    // el valor -99. El método devuelve mediante return el ArrayList con los valores introducidos.
+    public static ArrayList leerValores() {
+        Scanner input = new Scanner(System.in);
+        float numero = 0;
+        final float FIN = -99f;
+        ArrayList<Float> listaNumeros = new ArrayList<Float>();
+
+        while (numero != FIN) {
+            System.out.println("Ingrese un número: ");
+            numero = input.nextFloat();
+            // Asumo que el número -99 es sólo para terminar el ingreso de datos y no debe guardarse en el ArrayList
+            if (numero != FIN) {
+                listaNumeros.add(numero);
+            }
+        }
+        return listaNumeros;
+    }
 
 }
